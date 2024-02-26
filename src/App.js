b28b3e0c15d78a10d7a1fdde2abac2616c3aa1f6
souldvn <emilevil97@gmail.com>
2024-02-27 01:47:16 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import { useEffect } from 'react';
+import './App.css';
+
+const tg = window.Telegram.WebApp
+
+
+function App() {
+
+  useEffect (() =>{
+    tg.ready()
+  }, [])
+
+  const onClose = () =>{
+    tg.close
+  }
+
+
+  return (
+    <div className="App">
+      автв
+     <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+}
+
+export default App;
