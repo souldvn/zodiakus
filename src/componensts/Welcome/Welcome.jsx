16ca1d7de1e87530df22ababb4c613262b6abbef
souldvn <emilevil97@gmail.com>
2024-02-28 02:04:16 +0300

    init

--- src/componensts/Welcome/Welcome.jsx
+++ src/componensts/Welcome/Welcome.jsx
@@ -8,6 +8,17 @@ const Welcome = () => {
   return (
     <div className={s.main}>
         <img className={s.pic} src={welpic1} alt="Onboarding one" />
+        <div className={s.action}>
+          <div className={s.descript}>
+            <p className={s.title}>Ваш астрологический ассистент</p>
+            <p className={s.text}>Добро пожаловать! Здесь вы найдёте персонализированный прогноз для вашего знака зодиака. Узнайте, что звезды говорят о вашем будущем и получите ценные советы для своей жизни.</p>
+            <div className={s.buttons}>
+              <button>Далее</button>
+              <button>Пропустить</button>
+            </div>
+          </div>
+            
+        </div>
     </div>
   )
 }
