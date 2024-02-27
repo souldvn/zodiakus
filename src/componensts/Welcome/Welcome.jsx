f7722ac7380373f5f87b54186cb0088777494c42
souldvn <emilevil97@gmail.com>
2024-02-28 01:36:41 +0300

    init

--- src/componensts/Welcome/Welcome.jsx
+++ src/componensts/Welcome/Welcome.jsx
@@ -7,7 +7,7 @@ import welpic1 from '../../img/Onboardingone.svg'
 const Welcome = () => {
   return (
     <div className={s.main}>
-        <img src={welpic1} alt="Onboarding one" />
+        <img className={s.pic} src={welpic1} alt="Onboarding one" />
     </div>
   )
 }
