f50ce7366dcf48280b25f633097dad9b39ff9d3d
souldvn <emilevil97@gmail.com>
2024-03-17 02:45:18 +0300

    init

--- src/componensts/Birdth/Birdth.jsx
+++ src/componensts/Birdth/Birdth.jsx
@@ -12,17 +12,15 @@ const Birdth = () => {
   const navigate = useNavigate()
 
   const handleNextClick = () => {
-    if (dateValue){
-       navigate('/main') 
-    }
-    
+    if (dateValue)
+    navigate('/main')
   }
 
   return (
     <div className={s.birdth}>
         <div className={s.action}>
             <p>Укажите дату рождения</p>
-            <input className={s.date} type='date' value={dateValue} onChange={handleDateChange} />
+            <input className={s.date} type='date'  onChange={handleDateChange} />
         </div>
         <button className={s.next} style={{backgroundColor: dateValue ? '#7E46E8' : ''}} onClick={handleNextClick}>Далее</button>
     </div>
