da7cbd5028281a608a532eb4df0c17b20764cbb5
souldvn <emilevil97@gmail.com>
2024-03-18 20:13:26 +0300

    init

--- src/componensts/YourSign/Choose/Choose.jsx
+++ src/componensts/YourSign/Choose/Choose.jsx
@@ -15,57 +15,54 @@ import aquarius from '../../../sings/vodoley.svg';
 import pisces from '../../../sings/ryby.svg';
 
 const Choose = () => {
-
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const timer = setTimeout(() => {
-          setLoading(false);
+            setLoading(false);
         }, 4000);
-        
+
         return () => clearTimeout(timer);
-      }, []);
-      
-      return (
+    }, []);
+
+    return (
         <div className={s.choose}>
-          {loading ? (
-            <div className={st.loading}>
-              <div className={st.container}>
-                <div className={st.flex}>
-                  <div className={st.loader}></div>
+            <div className={st.loading} style={{ display: loading ? 'block' : 'none' }}>
+                <div className={st.container}>
+                    <div className={st.flex}>
+                        <div className={st.loader}></div>
+                    </div>
                 </div>
-              </div>
             </div>
-          ) : (
-            <>
-              <div className={s.sector}>
-                <img src={aries} alt="aries" />
-                <img src={taurus} alt="taurus" />
-              </div>
-              <div className={s.sector}>
-                <img src={gemini} alt="gemini" />
-                <img src={cancer} alt="cancer" />
-              </div>
-              <div className={s.sector}>
-                <img src={leo} alt="leo" />
-                <img src={virgo} alt="virgo" />
-              </div>
-              <div className={s.sector}>
-                <img src={libra} alt="libra" />
-                <img src={scorpio} alt="scorpio" />
-              </div>
-              <div className={s.sector}>
-                <img src={sagittarius} alt="sagittarius" />
-                <img src={capricorn} alt="capricorn" />
-              </div>
-              <div className={s.sector}>
-                <img src={aquarius} alt="aquarius" />
-                <img src={pisces} alt="pisces" />
-              </div>
-            </>
-          )}
+            <div style={{ display: loading ? 'none' : 'block' }}>
+                <div className={s.sector}>
+                    <img src={aries} alt="aries" />
+                    <img src={taurus} alt="taurus" />
+                </div>
+                <div className={s.sector}>
+                    <img src={gemini} alt="gemini" />
+                    <img src={cancer} alt="cancer" />
+                </div>
+                <div className={s.sector}>
+                    <img src={leo} alt="leo" />
+                    <img src={virgo} alt="virgo" />
+                </div>
+                <div className={s.sector}>
+                    <img src={libra} alt="libra" />
+                    <img src={scorpio} alt="scorpio" />
+                </div>
+                <div className={s.sector}>
+                    <img src={sagittarius} alt="sagittarius" />
+                    <img src={capricorn} alt="capricorn" />
+                </div>
+                <div className={s.sector}>
+                    <img src={aquarius} alt="aquarius" />
+                    <img src={pisces} alt="pisces" />
+                </div>
+            </div>
         </div>
-      );
+    );
 }
 
 export default Choose;
+
